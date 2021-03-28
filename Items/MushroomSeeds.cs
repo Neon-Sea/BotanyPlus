b9d231198a1af1f06915645d330502dc51e2d4f0
Neon-Sea <ts14054@gmail.com>
2021-03-28 00:38:21 -0600

    seeds

---
+++ Items/MushroomSeeds.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+using static Terraria.ModLoader.ModContent;
+
+namespace BotanyPlus.Items
+{
+    public class MushroomSeeds : ModItem
+    {
+        public override void SetDefaults()
+        {
+			item.width = 12;
+			item.height = 14;
+			item.maxStack = 99;
+			item.useTurn = true;
+			item.autoReuse = true;
+			item.useAnimation = 15;
+			item.useTime = 10;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.consumable = true;
+			item.value = 80;
+			item.createTile = TileType<Tiles.BotanyImmatureHerbs>();
+			item.placeStyle = 1;
+		}
+    }
+}
--- Tiles/BotanyBloomingHerbs.cs
+++ Tiles/BotanyBloomingHerbs.cs
@@ -3,7 +3,7 @@ using Terraria.ModLoader;
 using Terraria.ObjectData;
 using Terraria.ID;
 
-namespace BotanyPlus
+namespace BotanyPlus.Tiles
 {
     public class BotanyBloomingHerbs : ModTile
     {
